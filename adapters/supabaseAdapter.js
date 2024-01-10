9827f8c2b85a94056220cd80adc37f30f824e599
t0nkisa <antanitonev03@gmail.com>
2024-01-10 13:07:10 +0100

    making supa client and setting session

--- adapters/supabaseAdapter.js
+++ adapters/supabaseAdapter.js
@@ -2,18 +2,19 @@ import * as dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
 
 dotenv.config({ path: 'variables.env' });
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
 
-export async function getAnimalsData() {
+
+export async function getAnimalsData(supabase) {
   const { data, error } = await supabase.from('animals').select();
   if (error) console.log('query error', error);
   else return data;
 }
 
 export async function getAnimalById(id) {
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  );
   const { data, error } = await supabase.from('animals').select().eq('id',id).single();
   if (error) return {id:null};
   else return data;
--- controllers/animalsViaSupabase.js
+++ controllers/animalsViaSupabase.js
@@ -1,8 +1,21 @@
 import { getAnimalsData,getAnimalById } from "../adapters/supabaseAdapter.js";
-
+import { createClient } from '@supabase/supabase-js';
 export async function getAnimals(req, res) {
   try {
-    const data = await getAnimalsData();
+    const supabase = createClient(
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY,
+      {
+        global: {
+          headers: { "set-cookies": req.headers.cookie },
+        },
+      }
+    );
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    const data = await getAnimalsData(supabase);
+    console.log(data);
     res.status(200).json(data);
   } catch (err) {
     res.send(`error in viaSupabase: ${err}`);
